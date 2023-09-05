49df6d991404c68f0ceb2583174f306c84bc54ad
FABRIZZIO <FABRIZZIO@DESKTOP-8PLVDLM>
2023-09-05 15:24:20 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 3 c2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3_c2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingresa la edad del ganador: ");
+            int edad = int.Parse(Console.ReadLine());
+            Console.Write("ingresa la cantidad de adivinanzas que adivino: ");
+            int adivinanzasadivinadas = int.Parse(Console.ReadLine());
+
+            int montoinicial = 0;
+
+            if (edad >= 18)
+            {
+                if (adivinanzasadivinadas < 3)
+                    montoinicial = 30;
+                else
+                    montoinicial = 50;
+            }
+            else
+            {
+                if (adivinanzasadivinadas < 3)
+                    montoinicial = 50;
+                else
+                    montoinicial = 80;
+            }
+            int montoadicional = (edad * 5) + (adivinanzasadivinadas * 2);
+            int montofinal = montoinicial + montoadicional;
+            Console.WriteLine(" el monto final que el ganador gano es: " + montofinal);
+            Console.ReadKey();
+
+        }
+    }
+}
